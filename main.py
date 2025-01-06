e7c9aca7bfacf73312eb549380ee01559b817d75
billymeddleton <meddletonw@gmail.com>
2025-01-06 22:39:49 +0100

    fixed dotenv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from crypt import methods
 from datetime import date
+
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
